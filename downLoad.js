885fcd2f5cc95997c524098e7776018c23df6d73
shyer <bb1518959431@126.com>
2022-06-09 16:30:14 +0800

    新增利用日志补全文件功能

--- downLoad.js
+++ downLoad.js
@@ -2,34 +2,13 @@
 var axios = require('axios')
 var Bagpipe = require('bagpipe')
 var fs = require("fs");
-var { bou, Minlevel, Maxlevel, token, zpath, speed, mapstyles } = require('./config') // 引入参数
+var { user_agent_list_2, bou, Minlevel, Maxlevel, token, zpath, speed, mapstyles } = require('./config') // 引入参数
 var currentDate = new Date(); //日期处理
 var date = currentDate.getTime() // 计入启动时时间生成日志
 
 var sum = 0;
 var requestTotal = 0;
 var all = [];
-var user_agent_list_2 = [
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 OPR/26.0.1656.60",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0",
-    "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
-    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 Safari/534.16",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 SE 2.X MetaSr 1.0",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; SE 2.X MetaSr 1.0)",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.3.4000 Chrome/30.0.1599.101 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 UBrowser/4.0.3214.0 Safari/537.36"
-]
 
 /**
  * 计算经纬度转换成瓦片坐标
@@ -143,7 +122,7 @@ function download(x, y, z, mapstyle) {
                     'X-Forwarded-For': ip,
                     "Connection": 'keep-alive'
                 },
-                timeout: 30000,
+                timeout: 60000,
                 responseType: 'stream',
                 forever: true
             };
@@ -158,7 +137,11 @@ function download(x, y, z, mapstyle) {
                         err: err,
                         num: sum,
                         downloadNum: requestTotal,
-                        options: options
+                        options: options,
+                        mapstyle: mapstyle,
+                        z: z,
+                        x: x,
+                        y: y
                     }, date)
                     // process.exit(1)
                 }))
@@ -169,7 +152,11 @@ function download(x, y, z, mapstyle) {
                     err: err,
                     num: sum,
                     downloadNum: requestTotal,
-                    options: options
+                    options: options,
+                    mapstyle: mapstyle,
+                    z: z,
+                    x: x,
+                    y: y
                 }, date)
                 // process.exit(1)
             });
@@ -192,16 +179,26 @@ function appendLog(cusid, body, date) {
     let day = currentDate.getDate();
 
     //封装保存内容，'\n'为换行符
-    let text = currentDate.toLocaleString() + '\n' + cusid + '\n' + JSON.stringify(body) + '\n'
+    let text = currentDate.toLocaleString() + '\n' + cusid + '\n' + JSON.stringify(body) + '\n';
+    let supplementTest = JSON.stringify({
+        mapstyle: body.mapstyle,
+        z: body.z,
+        x: body.x,
+        y: body.y
+    }) + '\n';
 
-    let logFile = "./log/log_" + year + '_' + month + '_' + day + '_' + date + ".txt"
+    let logFile = "./log/log_" + year + '_' + month + '_' + day + '_' + date + ".txt"; // 标准日志文件
+    let supplementFile = "./log/log_" + year + '_' + month + '_' + day + '_' + date + "_supplement_urlLists.txt"; // 修复补全文件
     //存在则追加，不存在则新建
     if (fs.existsSync(logFile)) {
+        // 防止单日志文件大小过大
         if (fs.statSync(logFile).size > 102400) process.exit(1);
         fs.appendFile(logFile, text, (error) => {});
+        fs.appendFile(supplementFile, supplementTest, (error) => {});
     } else {
         console.log('该路径不存在');
         fs.writeFile(logFile, text, (error) => {});
+        fs.writeFile(supplementFile, supplementTest, (error) => {});
     }
 }
 
--- test.js
+++ test.js
@@ -1,30 +1,122 @@
-// An highlighted block
 var axios = require('axios')
 var fs = require("fs");
-var { bou, Minlevel, Maxlevel, token, zpath, speed, mapstyles } = require('./config') // 引入参数
+var Bagpipe = require('bagpipe')
+var { user_agent_list_2, bou, Minlevel, Maxlevel, token, zpath, speed, mapstyles } = require('./config') // 引入参数
 
+var currentDate = new Date(); //日期处理
+var date = currentDate.getTime() // 计入启动时时间生成日志
 var type = 'vec_w';
 var x = '1';
 var y = '1';
 var z = '1';
+var logPath = ''; // 如果填入 log 地址即使用 log 中错误请求对现有资源进行补充或修复 例：log/log_2022_6_9_1654761977895_supplement_urlLists.txt
 
-// 用于测试某个图片的请求
-axios({
-    method: 'GET',
-    url: `http://t0.tianditu.gov.cn/DataServer?T=${type}&x=${x}&y=${y}&l=${z}&tk=${token}`,
-    headers: {
-        'User-Agent': '"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0"',
-        'X-Forwarded-For': '182.11.56.20',
-        "Connection": 'keep-alive'
-    },
-    responseType: 'stream',
-    forever: true
-}).then(res => {
-    res.data.pipe(fs.createWriteStream(`test.png`).on('finish', () => {
-        console.log('成功');
-    }).on('error', (err) => {
-        console.log('写入异常', err);
-    }))
-}).catch(err => {
-    console.log('接口异常', err);
-});
+
+var bag = new Bagpipe(speed, { timeout: 1000 })
+
+function download(data) {
+    let params = JSON.parse(data)
+    var ip = Math.floor(Math.random() * 256) + "." + Math.floor(Math.random() * 256) + "." + Math.floor(Math.random() * 256) + "." + Math.floor(Math.random() * 256) //随机生成IP迷惑服务器
+    var v = Math.floor(Math.random() * 9)
+    var ts = Math.floor(Math.random() * 5) //随机生成0-7台服务器
+    let imgurl = `http://t${ts}.tianditu.gov.cn/DataServer?T=${params.mapstyle}&x=${params.x}&y=${params.y}&l=${params.z}&tk=${token}`;
+    var options = {
+        method: 'GET',
+        url: imgurl,
+        headers: {
+            'User-Agent': user_agent_list_2[v],
+            'X-Forwarded-For': ip,
+            "Connection": 'keep-alive'
+        },
+        timeout: 60000,
+        responseType: 'stream',
+        forever: true
+    };
+    axios(options).then(res => {
+        res.data.pipe(fs.createWriteStream(`${zpath}/${params.mapstyle}/${params.z}/${params.x}/${params.y}.png`).on('finish', () => {
+            console.log(`图片下载成功:${zpath}/${params.mapstyle}/${params.z}/${params.x}/${params.y}.png`);
+            appendLog('testWriteSuccess', {
+                options: options
+            }, date)
+        }).on('error', (err) => {
+            console.log('写入发生异常');
+            appendLog('testWriteErr', {
+                err: err,
+                options: options
+            }, date)
+            // process.exit(1)
+        }))
+    }).catch(err => {
+        console.log('请求异常:' + err);
+        appendLog('testRequestErr', {
+            err: err,
+            options: options
+        }, date)
+        // process.exit(1)
+    });
+}
+
+
+/**
+ * 
+ * @param {String} cusid 参数1:表示要向那个文件追加内容,只一个文件的路径
+ * @param {Object} body 参数2:表示要追加的内容
+ * @param {Date} date 参数3:时间戳生成文件名字
+ */
+function appendLog(cusid, body, date) {
+
+    let year = currentDate.getFullYear();
+    let month = currentDate.getMonth() + 1;
+    let day = currentDate.getDate();
+
+    //封装保存内容，'\n'为换行符
+    let text = currentDate.toLocaleString() + '\n' + cusid + '\n' + JSON.stringify(body) + '\n'
+
+    let logFile = "./log/testLog_" + year + '_' + month + '_' + day + '_' + date + ".txt"
+    //存在则追加，不存在则新建
+    if (fs.existsSync(logFile)) {
+        // 防止单日志文件大小过大
+        if (fs.statSync(logFile).size > 102400) process.exit(1);
+        fs.appendFile(logFile, text, (error) => {});
+    } else {
+        console.log('该路径不存在');
+        fs.writeFile(logFile, text, (error) => {});
+    }
+}
+
+
+
+
+
+fs.mkdir('./log', null, (error) => {});
+fs.readFile(logPath, 'utf-8', (err, stats) => {
+    if (err) {
+        // 文件不存在 测试某个图片的请求
+        axios({
+            method: 'GET',
+            url: `http://t0.tianditu.gov.cn/DataServer?T=${type}&x=${x}&y=${y}&l=${z}&tk=${token}`,
+            headers: {
+                'User-Agent': '"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0"',
+                'X-Forwarded-For': '192.168.0.1',
+                "Connection": 'keep-alive'
+            },
+            responseType: 'stream',
+            forever: true
+        }).then(res => {
+            res.data.pipe(fs.createWriteStream('test.png').on('finish', () => {
+                console.log('测试成功');
+            }).on('error', (err) => {
+                console.log('测试写入异常', err);
+            }))
+        }).catch(err => {
+            console.log('测试接口异常', err);
+        });
+    } else {
+        // 文件存在开始进行修复或补全
+        let urls = stats.split('\n')
+        urls.pop()
+        urls.forEach(data => {
+            bag.push(download, data)
+        })
+    }
+})
